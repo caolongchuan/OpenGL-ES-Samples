8a237ef762b0d9fe37d5226ef6eb3031c1f4003c
ModestBean <35394714+ModestBean@users.noreply.github.com>
2018-01-21 15:42:14 +0800

    Revert "modify frag"
    This reverts commit b0f09fdaf6d1924ccfb89e8cfffae1356297126a.

--- OneNDKTriangle/app/src/main/assets/shader/frag.sh
+++ OneNDKTriangle/app/src/main/assets/shader/frag.sh
@@ -4,6 +4,5 @@ in vec4 DestinationColor; //接收从顶点着色器过来的参数
 out vec4 fragColor;
 void main()
 {
-    //fragColor = DestinationColor; //给此片元从顶点着色器传过来的颜色值
-    fragColor = vec4(1.0,1.0,1.0,1.0); //给此片元从顶点着色器传过来的颜色值
+    fragColor = DestinationColor; //给此片元从顶点着色器传过来的颜色值
 }
